671a5147cb94d55d75abcc858be5c6df6f6178fe
Leighton Coughlin <leighton.coughlin1@gmail.com>
2024-04-23 23:40:31 +0100

    updated for april 2024

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 /*-------------- header hide/show --------------*/
 
-(function(){
-
-var doc = document.documentElement;
+(function () {
+  var doc = document.documentElement;
   var w = window;
 
   var prevScroll = w.scrollY || doc.scrollTop;
@@ -10,62 +9,54 @@ var doc = document.documentElement;
   var direction = 0;
   var prevDirection = 0;
 
-  var header = document.getElementById('navbar');
+  var header = document.getElementById("navbar");
 
-  var checkScroll = function() {
-
+  var checkScroll = function () {
     curScroll = w.scrollY || doc.scrollTop;
-    if (curScroll > prevScroll) { 
+    if (curScroll > prevScroll) {
       direction = 2;
-      console.log("scrolldown")
-    }
-
-    else if (curScroll < prevScroll) { 
+      console.log("scrolldown");
+    } else if (curScroll < prevScroll) {
       direction = 1;
-      console.log("scrollup")
+      console.log("scrollup");
     }
 
     if (direction !== prevDirection) {
       toggleHeader(direction, curScroll);
     }
-    
+
     prevScroll = curScroll;
   };
 
-  var toggleHeader = function(direction, curScroll) {
-    if (direction === 2 && curScroll > 100) { 
-    
-      header.classList.add('hide');
+  var toggleHeader = function (direction, curScroll) {
+    if (direction === 2 && curScroll > 100) {
+      header.classList.add("hide");
       prevDirection = direction;
-    }
-    else if (direction === 1) {
-      header.classList.remove('hide');
+    } else if (direction === 1) {
+      header.classList.remove("hide");
       prevDirection = direction;
     }
   };
-  
-  window.addEventListener('scroll', checkScroll);
+
+  window.addEventListener("scroll", checkScroll);
 })();
 
 /*----------  menu  ------------*/
 
 const bar = document.getElementById("bar");
 const close = document.getElementById("close");
-const navbar = document.querySelector(".header-div")
+const navbar = document.querySelector(".header-div");
 
 if (bar) {
-    bar.addEventListener("click", () => {
-        navbar.classList.add("active");
-        console.log("clicked")
-    })
+  bar.addEventListener("click", () => {
+    navbar.classList.add("active");
+    console.log("clicked");
+  });
 }
 
 if (close) {
-    close.addEventListener("click", () => {
-        navbar.classList.remove("active");
-        console.log("clicked")
-    })
+  close.addEventListener("click", () => {
+    navbar.classList.remove("active");
+    console.log("clicked");
+  });
 }
-
-
-
